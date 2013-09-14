3380a612b203ee4666a7f11485ed03813bd08c0f
Massimiliano Leone <maximilianus@gmail.com>
2013-09-14 15:57:47 +0200

    fixed license

--- src/prepare.js
+++ src/prepare.js
@@ -1,6 +1,22 @@
 /*
- * Massimiliano Leone - maximilianus@gmail.com - 2009, GPL licence
- */
+Copyleft 2013 Massimiliano Leone - maximilianus@gmail.com .
+
+prepare.js is part of 'kv'.
+
+'kv' is free software; you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation; either version 3 of the License, or
+(at your option) any later version.
+
+'kv' is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU Lesser General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with 'kv'; if not, write to the Free Software
+Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301 USA
+*/
 
 // this part is for the form field hints to display
 // only on the condition that the text input has focus.
